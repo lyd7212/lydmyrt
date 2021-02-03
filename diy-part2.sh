fb8f97d7f0c8acbea1179d10bc91b675124e8d54
SXSW <mydey@users.noreply.github.com>
2021-02-03 23:18:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
-sed -i 's/OpenWrt/GDock/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/G-Dock/g' package/base-files/files/bin/config_generate
 
 
 
