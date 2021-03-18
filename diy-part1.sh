36a3b4d7d5e9ff8bfaa873b546c9388a9e9135ae
tsoo <mydey@users.noreply.github.com>
2021-03-18 11:05:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,12 +7,16 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=============================================================
-# 增加https://github.com/kenzok8/openwrt-packages到更新源
+
 # src-git kenzo https://github.com/kenzok8/openwrt-packages
 
 # fw876/helloworld
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
 
+# linux kernel version
+# sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/ipq40xx/Makefile
+
+# add packages
 sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
