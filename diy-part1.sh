3ad1c1312308bf835d51a053f1eca883ba949e47
lyd7212 <43334340+lyd7212@users.noreply.github.com>
2021-03-19 15:46:57 +0800

    #获取Lieno1-xiaorouji-passiwal1
    #获取Lieno1-xiaorouji-passiwal1

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,9 @@
 # add packages
 sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#获取Lieno1-xiaorouji-passiwal1
+#git clone https://github.com/xiaorouji/openwrt-package/lieno1/package/diy-packages/1ienol
+#git clone https://github.com/xiaorouji/openwrt-package/tree/master/1ienol/luci-app-passwall package/1uci-app-passwal1
+git clone https://github.com/kenzok8/openwrt-packages.git package/diy-packages
+git clone https://github.com/kenzok8/smal1.git package/small
